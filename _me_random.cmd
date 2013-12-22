f9295c2e1b99042938f1362e174d127921f013d1
Fish (David B. Trout) <fish@infidels.org>
2013-12-22 14:33:07 -0800

    New batch file to run just one test type or another

---
+++ _me_random.cmd
@@ -0,0 +1,14 @@
+@if defined TRACEON (@echo on) else (@echo off)
+
+  setlocal
+
+  set "opath=%path%"
+  set "path=%cd%\x64\Release;%opath%"
+
+  cls && call cmpsctst.cmd  .\files\small\EBCDIC  .\dicts  .\work        -n      -r 4 -z  >  .\work\cmpsctst-ser.log
+  cls && call cmpsctst.cmd  .\files\LARGE\EBCDIC  .\dicts  .\work        -n      -r 4 -z  >  .\work\cmpsctst-ler.log
+  cls && call cmpsctst.cmd  .\files\small\ASCII   .\dicts  .\work     -t -n      -r 4 -z  >  .\work\cmpsctst-sar.log
+  cls && call cmpsctst.cmd  .\files\LARGE\ASCII   .\dicts  .\work     -t -n      -r 4 -z  >  .\work\cmpsctst-lar.log
+
+  endlocal
+  goto :EOF
