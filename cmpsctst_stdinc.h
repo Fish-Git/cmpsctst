fcbebdef34f2c252281ff2d04a322bf156d93bb2
Fish (David B. Trout) <fish@infidels.org>
2019-11-07 13:02:31 -0800

    pre-compiled header tweak, add new headers to vcproj/sln

--- cmpsctst_stdinc.h
+++ cmpsctst_stdinc.h
@@ -22,16 +22,55 @@
 #define SDL_HYPERION            /* Distinguish ourselves from others */
 #define NOT_HERC                /* This is the utility, not Hercules */
 
+/*-------------------------------------------------------------------*/
+/*                Linux (non-Windows) CONFIG header                  */
+/*-------------------------------------------------------------------*/
 #ifdef HAVE_CONFIG_H
   #ifndef    _CONFIG_H
   #define    _CONFIG_H
     #include <config.h>         /* Hercules build configuration      */
   #endif /*  _CONFIG_H*/
 #endif
-  #include "hqainc.h"           /* User build settings overrides     */
-#ifdef WIN32
+
+/*-------------------------------------------------------------------*/
+/*               HQA: User build settings overrides                  */
+/*-------------------------------------------------------------------*/
+#include "hqainc.h"             /* User build settings overrides     */
+
+/*-------------------------------------------------------------------*/
+/*                Normalize Windows Compiler flags                   */
+/*-------------------------------------------------------------------*/
+#if defined(_WIN32) || defined(WIN32)
+   /* Normalize WIN32/_WIN32 flags */
+#  if !defined(_WIN32) && defined(WIN32)
+#    define _WIN32
+#  elif defined(_WIN32) && !defined(WIN32)
+#    define WIN32
+#  endif
+   /* Normalize WIN64/_WIN64 flags */
+#  if defined(_WIN64) || defined(WIN64)
+#    if !defined(_WIN64) && defined(WIN64)
+#      define _WIN64
+#    elif defined(_WIN64) && !defined(WIN64)
+#      define WIN64
+#    endif
+#  endif
+   /* Normalize DEBUG/_DEBUG flags */
+#  if defined(_DEBUG) || defined(DEBUG)
+#    if !defined(_DEBUG) && defined(DEBUG)
+#      define _DEBUG
+#    elif defined(_DEBUG) && !defined(DEBUG)
+#      define DEBUG
+#    endif
+#  endif
+#endif /* defined(_WIN32) || defined(WIN32) */
+
+/*-------------------------------------------------------------------*/
+/*          TARGETVER: Minimum Supported Windows Platform            */
+/*-------------------------------------------------------------------*/
+#ifdef _WIN32
   #include "vsvers.h"           /* Visual Studio compiler constants  */
-  #include "targetver.h"        /* Minimum Windows platform          */
+  #include "targetver.h"        /* Minimum Supported Windows version */
 #endif
 
 /*-------------------------------------------------------------------*/
